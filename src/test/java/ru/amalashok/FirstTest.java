7342a3812d9571f78e4af767245e95231c5046f1
Antonina Malashok <antonina.demesh@yandex.ru>
2022-09-18 10:20:58 +0300

    first commit

---
+++ src/test/java/ru/amalashok/FirstTest.java
@@ -0,0 +1,12 @@
+package ru.amalashok;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void Test1 () {
+        Assertions.assertFalse(2 < 1);
+    }
+}
